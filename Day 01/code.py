878cdd56e0820fc9ec9cbcbd175f2478bc44dc35
Sri Hary <sriharym21@gmail.com>
2026-03-23 21:39:43 +0530

    Added Day01 code

--- Day 01/code.py
+++ Day 01/code.py
@@ -1,1 +1,45 @@
+#Variables and Data Types
 
+#name = input("Enter your name: ")
+#age = int(input("Enter your age: "))
+#print("Hello", name, "Your are", age, "year old")
+
+
+#Task
+
+#Declare variables for an integer (num_apples), a string (fav_fruit), a float (price), and a boolean (is_available).
+#Print each variable and its type.
+
+#num_apples = 3
+#fav_fruit = "apple"
+#price = 77.85
+#is_available = True
+
+#print("num_apples:", num_apples)
+#print("fav_fruit:", fav_fruit)
+#print("price:", price)
+#print("is_available:", is_available)
+
+
+#Create variables of different types and use them in a formatted string.
+#Create a variable name (string), age (integer), and is_student (boolean).
+#Create a variable height (float).
+#Print a single sentence that uses all four variables.
+
+#name = input("Enter your name: ")
+#age = int(input("Enter your age: "))
+#is_student = True
+#height = 5.9
+#print ("My name is", name, "I am", age, "years old, status of the student is", is_student, "and my height is", height)
+
+
+#Create a variable price with the value "199.99" (string).
+#Convert it into a float and then into an integer.
+#Print all three values (original, float, and integer) in one line.
+
+price = 199.99
+
+price_float = float(price)
+price_int = int(price_float)
+
+print("float:", price_float, "integer:", price_int)
